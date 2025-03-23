cf78ca37ac6a73e05bfe36d05b0ccd7d62dd5374
ChaoslordJames <jameswquinn@icloud.com>
2025-03-23 22:30:49 +0000

    Update ResilienceTests.swift

--- Tests/ResilienceTests.swift
+++ Tests/ResilienceTests.swift
@@ -5,6 +5,7 @@ class ResilienceTests: XCTestCase {
     func testExtremeLoad() async throws {
         let node = try await SelfEvolvingFractalGossipNode(peer_id: "test")
         let metrics = NetworkMetrics()
+        node.active_nodes.store(1000, ordering: .relaxed)
         XCTAssertGreaterThanOrEqual(metrics.resilience_score(node), 99.95)
     }
 }
