d6acfc3b8ad6bd93e94a80917932c9eb50e7c5ce
ChaoslordJames <jameswquinn@icloud.com>
2025-03-23 22:01:08 +0000

    Create NodeTests.swift

---
+++ Tests/NodeTests.swift
@@ -0,0 +1,15 @@
+import XCTest
+@testable import EliasChaosFractalApple
+
+class NodeTests: XCTestCase {
+    func testNodeInitialization() async throws {
+        let node = try await SelfEvolvingFractalGossipNode(peer_id: "test")
+        XCTAssertNotNil(node)
+    }
+
+    func testQueryProcessing() async throws {
+        let node = try await SelfEvolvingFractalGossipNode(peer_id: "test")
+        let response = await node.process_query("Hello")
+        XCTAssert(response.contains("v4.7.0"))
+    }
+}
